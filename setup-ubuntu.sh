4ca2859c34737ac810cc1289c498a7cee3b15add
Matt Colligan <matthewcolligan@improbable.io>
2023-08-30 20:54:38 +0100

    Revert "add x11proto ubnuntu deps"
    This reverts commit ed6e3660a1141003b86cf89024703b6f7f883cac.

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -25,22 +25,5 @@ sudo apt-get install -y --no-install-recommends \
     libxcb1-dev \
     libxkbcommon-dev \
     ninja-build \
-    x11proto-core-dev \
-    x11proto-dev \
-    x11proto-dri2-dev \
-    x11proto-fonts-dev \
-    x11proto-gl-dev \
-    x11proto-input-dev \
-    x11proto-kb-dev \
-    x11proto-present-dev \
-    x11proto-randr-dev \
-    x11proto-record-dev \
-    x11proto-render-dev \
-    x11proto-scrnsaver-dev \
-    x11proto-video-dev \
-    x11proto-xext-dev \
-    x11proto-xf86dga-dev \
-    x11proto-xf86dri-dev \
-    x11proto-xf86vidmode-dev \
-    x11proto-xinerama-dev
+    xwayland
 sudo pip install meson

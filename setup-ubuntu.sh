9573db12ad2c9aae68d6a11f3ea0b05d3a5a2d37
Matt Colligan <matthewcolligan@improbable.io>
2023-08-30 15:48:25 +0100

    Rework action input and ubuntu setup script

---
+++ setup-ubuntu.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Set up packaged dependencies on Ubuntu
+
+apt update
+apt-get install -y --no-install-recommends \
+    libegl1-mesa-dev \
+    libgbm-dev \
+    libgles2-mesa-dev \
+    libinput-dev \
+    libpciaccess-dev \
+    libxcb-composite0-dev \
+    libxcb-dri3-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-present-dev \
+    libxcb-render0-dev \
+    libxcb-res0-dev \
+    libxcb-xfixes0-dev \
+    libxcb-xinput-dev \
+    libxcb1-dev \
+    libxkbcommon-dev \
+    ninja-build \
+    xwayland
+pip install meson
